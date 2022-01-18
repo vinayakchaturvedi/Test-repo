d6d1de29a2c390a8906b57f8073e280be3b5070b
Vinayak <vinayak.chaturvedi96@gmail.com>
2022-01-18 11:05:53 +0530

    Adding Project created from Eclipse

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author cvina
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Demo");
+	}
+
+}
